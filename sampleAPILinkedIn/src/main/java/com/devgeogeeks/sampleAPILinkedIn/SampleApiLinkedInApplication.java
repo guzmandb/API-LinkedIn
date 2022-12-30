f5aad6c85c3f0314d1f901b8adfa10ba3bd36d17
guzmandb <46854359+guzmandb@users.noreply.github.com>
2022-12-30 11:38:07 -0500

    Add files via upload

---
+++ sampleAPILinkedIn/src/main/java/com/devgeogeeks/sampleAPILinkedIn/SampleApiLinkedInApplication.java
@@ -0,0 +1,13 @@
+package com.devgeogeeks.sampleAPILinkedIn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleApiLinkedInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleApiLinkedInApplication.class, args);
+	}
+
+}
---
+++ sampleAPILinkedIn/src/test/java/com/devgeogeeks/sampleAPILinkedIn/SampleApiLinkedInApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devgeogeeks.sampleAPILinkedIn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleApiLinkedInApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
